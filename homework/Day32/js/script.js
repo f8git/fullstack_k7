52c6fb9c96a4e9dc4835010a59c835dfe656335b
f8git <lmt1721999abc@gmail.com>
2024-06-18 14:55:43 +0700

    Nop bai tap ngay 32

--- homework/Day32/js/script.js
+++ homework/Day32/js/script.js
@@ -1,3 +1,23 @@
+var listProduct = [
+  { name: "Sản phẩm 1", cost: 1000 },
+  { name: "Sản phẩm 2", cost: 2000 },
+  { name: "Sản phẩm 3", cost: 3000 },
+  { name: "Sản phẩm 4", cost: 4000 },
+];
+
+var productTable = document.querySelector(".product-table");
+for (var i = 0; i < listProduct.length; i++) {
+  var tr = document.createElement("tr");
+  tr.innerHTML = `<td>${i + 1}</td>
+        <td>${listProduct[i].name}</td>
+        <td>${listProduct[i].cost}</td>
+        <td>
+          <input type="number" value="1" min="0"/>
+          <button class="btn btn1">Thêm vào giỏ</button>
+        </td>`;
+  productTable.append(tr);
+}
+
 var buttons = document.querySelectorAll("button.btn");
 var bodyCart = document.querySelector(".body-cart");
 var trLast = document.querySelector(".tr-last");
@@ -6,9 +26,25 @@ var btnUpdate = document.querySelector(".btn-update");
 var btnDelete = document.querySelector(".btn-delete");
 var cartTable = document.querySelector(".cart");
 var cartTextContent = document.querySelector(".text-content");
-console.log(buttons);
 
-var arrCart = [];
+// console.log(buttons);
+
+var arrCart;
+
+var setLocalStorage = function (arr) {
+  var json = JSON.stringify(arrCart);
+  // console.log(json.constructor.name);
+  localStorage.setItem("data", json);
+};
+
+var getLocalStorage = function () {
+  var json = localStorage.getItem("data");
+  var arr = JSON.parse(json);
+  return arr;
+};
+
+arrCart = getLocalStorage();
+console.log(arrCart);
 
 var renderCart = function (arr) {
   arr.forEach(function (item, index) {
@@ -25,6 +61,7 @@ var renderCart = function (arr) {
         var input = document.createElement("input");
         input.value = +itemValue[i];
         input.type = "number";
+        input.min = "0";
         td.append(input);
       } else if (i === 4) {
         td.innerText = `${+itemValue[i - 2] * +itemValue[i - 1]}`;
@@ -48,16 +85,21 @@ var renderCart = function (arr) {
         +bodyCart.children[i].children[3].children[0].value *
         +bodyCart.children[i].children[2].innerText;
       total += +bodyCart.children[i].children[3].children[0].value;
-      console.log(i);
-      console.log(bodyCart.children[0]);
+      // console.log(i);
+      // console.log(bodyCart.children[0]);
       prices += +bodyCart.children[i].children[4].innerText;
     }
-    console.log(prices);
+    // console.log(prices);
   } else {
   }
   trFooter.children[0].children[1].innerText = total;
   trFooter.children[0].children[2].innerText = prices;
 };
+renderCart(arrCart);
+if (arrCart.length) {
+  cartTextContent.style.display = "none";
+  cartTable.style.display = "block";
+}
 
 var dataAdd = function (btn) {
   var tr = btn.parentElement.parentElement.children;
@@ -80,6 +122,8 @@ var dataAdd = function (btn) {
     };
 
     arrCart.push(object);
+    setLocalStorage(arrCart);
+    // console.log(getLocalStorage());
 
     var trNew = document.createElement("tr");
     for (var i = 0; i < 6; i++) {
@@ -112,6 +156,9 @@ var dataAdd = function (btn) {
         arrCart[i].sl += +tr[3].children[0].value;
         bodyCart.children[i].children[3].children[0].value = arrCart[i].sl;
         arrCart[i].price = +tr[3].children[0].value * +tr[2].innerText;
+        setLocalStorage(arrCart);
+        // json = JSON.stringify(arrCart);
+        // console.log(json);
 
         break;
       }
@@ -134,21 +181,21 @@ buttons.forEach(function (btn) {
           +bodyCart.children[i].children[3].children[0].value *
           +bodyCart.children[i].children[2].innerText;
         total += +bodyCart.children[i].children[3].children[0].value;
-        console.log(i);
-        console.log(bodyCart.children[0]);
+        // console.log(i);
+        // console.log(bodyCart.children[0]);
         prices += +bodyCart.children[i].children[4].innerText;
       }
-      console.log(prices);
+      // console.log(prices);
     } else {
     }
     trFooter.children[0].children[1].innerText = total;
     trFooter.children[0].children[2].innerText = prices;
-    console.log(trFooter.children[0].children[1].innerText);
+    // console.log(trFooter.children[0].children[1].innerText);
   });
 });
 
 btnUpdate.addEventListener("click", function (e) {
-  console.log(e);
+  // console.log(e);
   alert("Cập nhật thành công");
   var total = 0;
   var prices = 0;
@@ -156,20 +203,34 @@ btnUpdate.addEventListener("click", function (e) {
   if (bodyCart.children.length > 0) {
     for (var i = 0; i < bodyCart.children.length; i++) {
       arrCart[i].sl = +bodyCart.children[i].children[3].children[0].value;
+
       bodyCart.children[i].children[4].innerText =
         +bodyCart.children[i].children[3].children[0].value *
         +bodyCart.children[i].children[2].innerText;
+
       total += +bodyCart.children[i].children[3].children[0].value;
-      console.log(i);
-      console.log(bodyCart.children[0]);
+      // console.log(i);
+      // console.log(bodyCart.children[0]);
       prices += +bodyCart.children[i].children[4].innerText;
+      console.log(bodyCart.children[i].children[4].innerText);
+      if (!+bodyCart.children[i].children[4].innerText) {
+        arrCart.splice(i, 1);
+        console.log(arrCart);
+        setLocalStorage(arrCart);
+        bodyCart.innerHTML = "";
+        renderCart(arrCart);
+        if (!arrCart.length) {
+          cartTextContent.style.display = "block";
+          cartTable.style.display = "none";
+        }
+      }
     }
-    console.log(prices);
+    // console.log(prices);
   } else {
   }
   trFooter.children[0].children[1].innerText = total;
   trFooter.children[0].children[2].innerText = prices;
-  console.log(trFooter.children[0].children[1].innerText);
+  // console.log(trFooter.children[0].children[1].innerText);
 });
 
 btnDelete.addEventListener("click", function () {
@@ -178,6 +239,7 @@ btnDelete.addEventListener("click", function () {
     cartTable.style.display = "none";
     bodyCart.innerHTML = "";
     arrCart = [];
+    setLocalStorage(arrCart);
     renderCart(arrCart);
   }
 });
@@ -185,11 +247,12 @@ btnDelete.addEventListener("click", function () {
 bodyCart.addEventListener("click", function (e) {
   if (e.target.localName === "button") {
     if (confirm("Are you sure?")) {
-      console.log(e.target.parentElement.parentElement.children[0]);
+      // console.log(e.target.parentElement.parentElement.children[0]);
       arrCart.splice(
         e.target.parentElement.parentElement.children[0].innerText - 1,
         1
       );
+      setLocalStorage(arrCart);
       bodyCart.innerHTML = "";
       renderCart(arrCart);
       if (!arrCart.length) {
